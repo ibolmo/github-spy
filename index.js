15baafc55a5530fc60fe02e926505b07cae88517
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-08-11 11:48:01 -0500

    working simple last id imp

--- index.js
+++ index.js
@@ -1,56 +1,94 @@
-var GithubAPI = require('node-github');
-var oboe = require('oboe');
-var fs = require('fs');
-var Q = require('q');
-var EventEmitter = require('events');
-var mongoose = require('mongoose');
+var GithubAPI = require('github');
 var debug = require('debug')('app');
-
-const DB_NAME = process.env.DB_NAME;
-const MONGO_PASS = process.env.MONGO_DB_PASS;
-const MONGO_HOST = process.env.OPENSHIFT_MONGODB_DB_HOST;
-const MONGO_PORT = process.env.OPENSHIFT_MONGODB_DB_PORT;
-
-if (MONGO_HOST) {
-  mongoose.connect('mongodb://admin:' + MONGO_PASS + '@' + MONGO_HOST + ':' + MONGO_PORT + '/' + DB_NAME);
-} else {
-  mongoose.connect('mongodb://localhost/' + DB_NAME);
-}
-
-var radio = new EventEmitter();
-
-var Event = mongoose.model('Event', { user: String, id: String, created_at: String });
+var fs = require('fs');
+var KeenTracking = require('keen-tracking');
 
 require('dotenv').config();
 
-var github = new GithubAPI({
-  debug: process.env.DEBUG,
-  version: '3.0.0'
+var keen = new KeenTracking({
+  projectId: process.env.KEEN_ID,
+  writeKey: process.env.KEEN_KEY
 });
 
-var handleEvents = function(err, res){
-  if (err) throw new Error(err);
+var github = new GithubAPI({
+  debug: process.env.DEBUG
+});
 
-  var last = res.slide(-1)[0];
-  Event.findOne({ id: last.id }, function(err, hasUnseenLast){
-    var events = res.map(function(e){
-      radio.emit('event', e);
-      return { user: 'ibolmo', id: e.id, created_at: e.created_at };
-    });
+github.authenticate({
+    type: "oauth",
+    key: process.env.GITHUB_ID,
+    secret: process.env.GITHUB_SECRET
+});
 
-    Event.collection.insert(events, function(err, docs){
-      if (err) throw new Error(err);
-      debug('Inserted ' + docs.length + ' events');
-    });
+var USERS = [
+  'avilano',
+  'atamalatzi',
+  'erecinos',
+  'jllanas',
+  'javmarr',
+  'joelgarza',
+  'pamsny',
+  'riccochapa',
+  'samcio',
+  'stevealvaradorgv'
+];
 
-    if (github.hasNextPage(res) && hasUnseenLast){
-      github.getNextPage(res, handleEvents);
-    }
-  });
+if (false && !process.env.OPENSHIFT_NODEJS_IP) {
+  USERS = ['ibolmo'];
 }
 
-github.events.getFromUser({ user: 'ibolmo' }, handleEvents);
+var db = {};
+if (fs.existsSync('./db.json')){
+  try {
+    db = JSON.parse(fs.readFileSync('./db.json'));
+  } catch(e){
+    debug(e);
+  }
+}
 
-radio.on('event', function(e){
+var handling = {};
 
+USERS.forEach(function(user){
+  handling[user] = true;
+
+  var handleEvents = function(err, res){
+
+    if (err) throw new Error(err);
+
+    var lastSeen = db[user];
+    var earliest = res.slice(-1)[0];
+    var needsToFetchMore = earliest && (Number(earliest) > Number(lastSeen));
+    debug(user + (needsToFetchMore ? ' needs to fetch more' : ' has no more events'));
+    debug('found ' + res.length + ' events');
+
+    res.reverse().forEach(function(e){
+      if (Number(e.id) > Number(db[user] || 0)) {
+        db[user] = e.id;
+        var payload = {
+          user: user,
+          keen: { timestamp: e.create_at },
+          actor: e.actor.login,
+          repo: e.repo.name
+        };
+        debug(user + ' (' + e.type + ') ' + JSON.stringify(payload, null, '  '));
+        keen.recordEvent(e.type, payload);
+      }
+    });
+
+    if (github.hasNextPage(res) && needsToFetchMore){
+      debug('Getting next page: ' + res.link);
+      github.getNextPage(res, handleEvents);
+    } else {
+      debug('Done. Last event: ' + db[user]);
+      fs.writeFileSync('./db.json', JSON.stringify(db, null, '  '));
+      delete handling[user];
+
+      if (!Object.keys(handling).length){
+        debug('all work completed');
+        process.exit();
+      }
+    }
+  };
+
+  github.activity.getEventsForUser({ user: user, per_page: 100 }, handleEvents);
 });
